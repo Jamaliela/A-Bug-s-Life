0589e2647721ab6172a165638882d2b45673ff40
jamaliela <Jamalie@berea.edu>
2018-09-25 20:58:29 -0400

    Making the code do something with the life path number result.

--- A04 - Jamalie - Alfaroz.py
+++ A04 - Jamalie - Alfaroz.py
@@ -8,122 +8,103 @@
 # Learn to use modulus to capture remainders.
 #
 ######################################################################
-# Acknowledgements: Aaron Christon and Eureka (the moment Ela had when she figured the first function out)
+# Acknowledgements: Aaron Christon (TA hours) and Eureka (the moment Ela had when she figured out the first function)
 ######################################################################
-
+import turtle
 
 def day_of_birth():
     """
-
-    :param
-    :return: None
+    this function first checks if the input is a master number (11,22,33) and returns it. if not a master number, it adds the integers of the day of birth and returns them.
+    :return: sum of integers of the day of birth or master number (11, 22 or 33)
     """
-    day = (int(input("What is the day of your birthday?")))
-    if day % 11 == 0 and day <= 33:
-        return day
+    day = (int(input("What is the day of your birthday?")))    # asks the user to input their day of birth
+    if day % 11 == 0 and day <= 33:                            # checks if input is a master number by dividing it by 11 and checking if it is less than 33
+        return day                                             # if the conditions are met, the master number is return (11,22,33)
     else:
-        n = day % 10
-        m = day // 10
-        return m + n
+        n = day % 10                                           # defining variable n and using modulus to get the second value to sum
+        m = day // 10                                          # defining variable m and using floor division to get to get the first value to sum
+        return m + n                                           # adding the two variables
 
 
 def month_of_birth():
     """
-
-    :param
-    :return: None
+    this function first checks if the input is a master number (11,22,33) and returns it. if not a master number, it adds the integers of the month of birth and returns them.
+    :return: sum of integers of the month of birth or master number (11, 22 or 33)
     """
-    month = (int(input("What is the month of your birthday?")))
-    if month % 11 == 0 and month <= 33:
-        return month
+    month = (int(input("What is the month of your birthday?")))   # asks the user to input their month of birth
+    if month % 11 == 0 and month <= 33:                           # checks if input is a master number by dividing it by 11 and checking if it is less than 33
+        return month                                              # if the conditions are met, the master number is return (11,22,33)
     else:
-        n = month % 10
-        m = month // 10
-        return m + n
+        n = month % 10                                            # defining variable n and using modulus to get the second value to sum
+        m = month // 10                                           # defining variable m and using floor division to get to get the first value
+        return m + n                                              # adding the two variables
 
 
 def year_of_birth():
     """
-
-    :return:
+    this function first checks if the input is a master number (11,22,33) and returns it. if not a master number, it adds the integers of the year of birth and returns them.
+    :return: sum of integers of the year of birth or master number (11, 22 or 33)
     """
-    year = (int(input("What is the year of your birthday?")))
-    if year % 11 == 0 and year <= 33:
-        return year
+    year = (int(input("What is the year of your birthday?")))     # asks the user to input their year of birth
+    if year % 11 == 0 and year <= 33:                             # checks if input is a master number by dividing it by 11 and checking if it is less than 33
+        return year                                               # if the conditions are met, the master number is return (11,22,33)
     else:
-        f1 = year // 1000
-        f2 = year % 1000
-        s1 = f2 // 100
-        s2 = f2 % 100
-        t1 = s2 // 10
-        t2 = s2 % 10
-        suma = f1 + s1 + t1 + t2
-        if suma >= 10 and suma % 11 != 0:
-            return suma % 10 + suma // 10
+        f1 = year // 1000                                         # using floor division by 1000 to get first integer for year
+        f2 = year % 1000                                          # reducing the year to hundreds so we can get second digit
+        s1 = f2 // 100                                            # using floor division by 100 to get second integer for year
+        s2 = f2 % 100                                             # reducing the year to tents so we can get third digit
+        t1 = s2 // 10                                             # using floor division by 10 to get third integer for year
+        t2 = s2 % 10                                              # reducing the year to tents so we can get remainder (fourth digit)
+        suma = f1 + s1 + t1 + t2                                  # adding all the variables previously found so we get the total for year
+        if suma >= 10 and suma % 11 != 0:                         # we calculate here if our value is equal or less than ten and aif the remainder of dividing it by 11 is not zero
+            return suma % 10 + suma // 10                         # if the conditions are met, we take the reminder of suma/10 and add it to the floor division of that number and 10
         else:
-            return suma
+            return suma                                           # if the final value does not meet the conditions, it is returned as it is.
 
 
 def life_path_number():
-    day1 = day_of_birth()
-    month1 = month_of_birth()
-    year1 = year_of_birth()
-    number = day1 + month1 + year1
-    if number >= 10 and number % 11 != 0:
-        return number % 10 + number // 10
+    """
+    function that first calls all the functions and then adds all their values found to get the final life path number
+    :return: sum of the three values found
+    """
+    day1 = day_of_birth()                                        # assigning the day_of_birth function to a variable to get its value
+    month1 = month_of_birth()                                    # assigning the month_of_birth function to a variable to get its value
+    year1 = year_of_birth()                                      # assigning the year_of_birth function to a variable to get its value
+    number = day1 + month1 + year1                               # adding the three values together to get the life path number
+    if number >= 10 and number % 11 != 0:                        # if the number found is equal or more than 10 and its reminder is other than zero then
+        return number % 10 + number // 10                        # the reminder of dividing the number by then and the one of doing floor division by ten is added.
     else:
-        return number
+        return number                                            # if those conditions are not met, the total amount (number) is returned.
 
 
+def final(tortuga):
+    """
 
-
-
-#
-# def pp ():
-#     """
-#
-#     :return: None
-#     """
-#     pass
-#
-# def
-#     """
-#
-#     :param num:
-#     :return:
-#     """
-#    pass
-#
-# def
-#     """
-#
-#
-#     :param
-#     :return: None
-#     """
-#
-#
-# def random_peeler():
-#     """
-#
-#     :return: None
-#     """
-#     pass
+    :return:
+    """
+    numero = life_path_number()
+    # numero == 6:
+    tortuga.write("Congratulations. Your life path number is " + str(numero), move=False, align="left", font=("Arial", 10, "normal"))
+    # else:
+    #     print("too bad")
 
 
 def main():
     """
-
-    :return: None
+    main function where other functions are executed to find the life path number
+    :return: life path number
     """
 
 
-# day_of_birth()
-# month_of_birth()
-# year_of_birth()
-print(life_path_number())
+    # (life_path_number())                                      # printing the final function where all other functions are ran and final value is found.
+    tortuga = turtle.Turtle()
+    wn = turtle.Screen()
+    final(tortuga)
+
+    wn.exitonclick()
 
 
 main()
+
 
 
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # A04: A Bug's Life
 
-Name 1: ____________
+Name 1: Emely Alfaro
 
-Name 2 (if needed): ____________
+Name 2 (if needed): Ela Jamali
 
-Repository Link: ______________
+Repository Link: https://github.com/fall-2018-csc-226/a04-a-bug-s-life-alfaro-jamali-a03.git
 
-Google Document Link: ______________
+Google Document Link: https://docs.google.com/document/d/1-QmVrysxwOZ6XfjiqdRo2cTn4_7562v8kEoXxD3TcNE/edit?usp=sharing
